f67a52fd892f1501191cd97e6d507d7da0feaa0d
rxshabN <nagwanirishab@gmail.com>
2024-12-01 08:09:15 +0530

    skills fix

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { useSectionInView } from "@/lib/hooks";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 import Image from "next/image";
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { skillsImages } from "@/lib/data";
 
 const fadeInAnimationVariants = {
@@ -11,23 +11,34 @@ const fadeInAnimationVariants = {
     opacity: 0,
     y: 100,
   },
-  animate: (index: number) => {
-    return {
-      opacity: 1,
-      y: 0,
-      transition: {
-        delay: 0.04 * index,
-      },
-    };
-  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.04 * index,
+    },
+  }),
 };
 
 export default function Skills() {
-  const { ref } = useSectionInView("Skills", 0.5);
+  const { ref: inViewRef } = useSectionInView("Skills", 0.5);
+  const sectionRef = useRef<HTMLElement>(null);
+  const isInView = useInView(sectionRef, { amount: 0.4 });
+  const [hasAnimated, setHasAnimated] = useState(false);
+  useEffect(() => {
+    if (isInView && !hasAnimated) {
+      setHasAnimated(true);
+    }
+  }, [isInView, hasAnimated]);
+  useEffect(() => {
+    if (inViewRef && typeof inViewRef === "function") {
+      inViewRef(sectionRef.current);
+    }
+  }, [inViewRef]);
   return (
     <section
       className="poppins mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40 relative"
-      ref={ref}
+      ref={sectionRef}
       id="skills"
     >
       <div className="sm:block hidden bg-[#6f61f3]/[0.3] absolute -z-10 top-[-1rem] left-[-75rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
@@ -48,7 +59,7 @@ export default function Skills() {
             key={skill.name}
             variants={fadeInAnimationVariants}
             initial="initial"
-            whileInView="animate"
+            animate={hasAnimated ? "animate" : "initial"} // Animate only if hasn't happened before
             viewport={{ once: true }}
             custom={index}
             className="hover:bg-gray-600/70 transition-colors flex flex-col items-center justify-center bg-gray-400/60 border border-black/[0.1] rounded-xl sm:px-5 sm:py-3 px-1 py-1 shadow-md"
